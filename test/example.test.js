a05fd473e80e2850ef999a3ac4b81237d9e2bdc1
Elise Muellerleile <eliamue@live.com>
2021-05-05 18:45:04 -0500

    test no longer broken

--- test/example.test.js
+++ test/example.test.js
@@ -1,19 +1,19 @@
 // IMPORT MODULES under test here:
-import { evaluateGuess } from '../utils.js'
+import { rpsCompare } from '../utils.js';
 
 const test = QUnit.test;
 
 test('return win if user defeats computer with rock', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expectedRock = true;
-    const expectedPaper = true;
-    const expectedScissors = true;
+    const expectedRock = 'win';
+    const expectedPaper = 'win';
+    const expectedScissors = 'win';
     //Act 
     // Call the function you're testing and set the result to a const
-    const actualRock = didUserGuessCorrect('rock', 'scissors');
-    const actualPaper = didUserGuessCorrect('paper', 'rock');
-    const actualScissors = didUserGuessCorrect('scissors', 'paper');
+    const actualRock = rpsCompare('rock', 'scissors');
+    const actualPaper = rpsCompare('paper', 'rock');
+    const actualScissors = rpsCompare('scissors', 'paper');
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actualRock, expectedRock);
