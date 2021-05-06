a118906d641314b60b5bab74d1acbcbd34f89e54
Elise Muellerleile <eliamue@live.com>
2021-05-06 12:09:14 -0500

    reset button added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import { rpsThrow, rpsCompare } from './utils.js';
 //grab DOM elements
 const button = document.getElementById('submit-button');
 const totalThrowsAndWinsDisplay = document.getElementById('throws-and-wins');
+const reset = document.getElementById('reset-button');
 
 // initialize state
 let totalThrows = 0;
@@ -19,14 +20,26 @@ button.addEventListener('click', () => {
     if (rpsCompare(userThrow, rpsThrow(randomNumber)) === 'win') {
         totalThrows ++;
         totalWins ++;
+        totalThrowsAndWinsDisplay.style.display = 'block';
         totalThrowsAndWinsDisplay.textContent = `WINNER! Yo you won like ${totalWins} out of ${totalThrows} times! Congrats, mate!`;
     } else if (rpsCompare(userThrow, rpsThrow(randomNumber)) === 'draw') {
         totalThrows ++;
         totalDraws ++;
+        totalThrowsAndWinsDisplay.style.display = 'block';
         totalThrowsAndWinsDisplay.textContent = `Aw, that's was a draw. You've had ${totalDraws} draws and still won ${totalWins} out of ${totalThrows} times, so try not to be terrible next time.`;
     } else if (rpsCompare(userThrow, rpsThrow(randomNumber)) === 'lose') {
         totalThrows ++;
         totalLosses ++;
+        totalThrowsAndWinsDisplay.style.display = 'block';
         totalThrowsAndWinsDisplay.textContent = `Looooooser, you lost. Seriously, just ${totalWins} wins out of ${totalThrows} games? You've lost ${totalLosses} times!? Make better choices.`;
+        reset.style.display = 'block';
     }
+});
+
+reset.addEventListener('click', () => {
+    totalThrows = 0;
+    totalDraws = 0;
+    totalWins = 0;
+    totalLosses = 0;
+    totalThrowsAndWinsDisplay.style.display = 'none';
 });
