50a921041c036010d53df87707a0e7a978d1b8c9
Elise Muellerleile <eliamue@live.com>
2021-05-06 12:23:16 -0500

    all outcomes displayed for each outcome display

--- app.js
+++ app.js
@@ -23,19 +23,19 @@ button.addEventListener('click', () => {
         totalThrows ++;
         totalWins ++;
         totalThrowsAndWinsDisplay.style.display = 'block';
-        totalThrowsAndWinsDisplay.textContent = `WINNER! Yo you won like ${totalWins} out of ${totalThrows} times! Congrats, mate!`;
+        totalThrowsAndWinsDisplay.textContent = `WINNER! Yo you won ${totalWins} out of ${totalThrows} times! Congrats, mate! You've had ${totalDraws} draws and ${totalLosses} losses, so let's hope you keep winning!`;
         reset.style.display = 'none';
     } else if (rpsCompare(userThrow, rpsThrow(randomNumber)) === 'draw') {
         totalThrows ++;
         totalDraws ++;
         totalThrowsAndWinsDisplay.style.display = 'block';
-        totalThrowsAndWinsDisplay.textContent = `Aw, that's was a draw. You've had ${totalDraws} draws and still won ${totalWins} out of ${totalThrows} times, so try not to be terrible next time.`;
+        totalThrowsAndWinsDisplay.textContent = `Aw, that's was a draw. You've had ${totalDraws} draws and still won ${totalWins} out of ${totalThrows} times, so try not to be terrible next time or add to your loss total of ${totalLosses}.`;
         reset.style.display = 'none';
     } else if (rpsCompare(userThrow, rpsThrow(randomNumber)) === 'lose') {
         totalThrows ++;
         totalLosses ++;
         totalThrowsAndWinsDisplay.style.display = 'block';
-        totalThrowsAndWinsDisplay.textContent = `Looooooser, you lost. Seriously, just ${totalWins} wins out of ${totalThrows} games? You've lost ${totalLosses} times!? Make better choices.`;
+        totalThrowsAndWinsDisplay.textContent = `Looooooser, you lost. Seriously, just ${totalWins} wins out of ${totalThrows} games? You've lost ${totalLosses} times!? Make better choices. You've even had ${totalDraws} draws, and we are not amused.`;
         reset.style.display = 'block';
     }
 });
