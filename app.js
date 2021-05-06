f02a2dbb72dd35cae1e0667833024230d499499c
Elise Muellerleile <eliamue@live.com>
2021-05-05 20:56:57 -0500

    now tracks number of all wins/loses/draws

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const totalThrowsAndWinsDisplay = document.getElementById('throws-and-wins');
 // initialize state
 let totalThrows = 0;
 let totalWins = 0;
+let totalLosses = 0;
+let totalDraws = 0;
 
 // set event listeners to update state and DOM
 button.addEventListener('click', () => {
@@ -20,9 +22,11 @@ button.addEventListener('click', () => {
         totalThrowsAndWinsDisplay.textContent = `WINNER! Yo you won like ${totalWins} out of ${totalThrows} times! Congrats, mate!`;
     } else if (rpsCompare(userThrow, rpsThrow(randomNumber)) === 'draw') {
         totalThrows ++;
-        totalThrowsAndWinsDisplay.textContent = `Aw, that's was a draw. You've still won ${totalWins} out of ${totalThrows} times, so try not to be terrible next time.`;
+        totalDraws ++;
+        totalThrowsAndWinsDisplay.textContent = `Aw, that's was a draw. You've had ${totalDraws} draws and still won ${totalWins} out of ${totalThrows} times, so try not to be terrible next time.`;
     } else if (rpsCompare(userThrow, rpsThrow(randomNumber)) === 'lose') {
         totalThrows ++;
-        totalThrowsAndWinsDisplay.textContent = `Looooooser, you lost. Seriously, just ${totalWins} wins out of ${totalThrows} games? Make better choices.`;
+        totalLosses ++;
+        totalThrowsAndWinsDisplay.textContent = `Looooooser, you lost. Seriously, just ${totalWins} wins out of ${totalThrows} games? You've lost ${totalLosses} times!? Make better choices.`;
     }
 });
